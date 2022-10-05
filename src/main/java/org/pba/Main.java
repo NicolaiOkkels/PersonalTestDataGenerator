fa24088dc36c0227ae4d3ed2100b5a53c8f52445
NicolaiOkkels <nico902t@stud.kea.dk>
2022-10-05 20:05:09 +0200

    added gen birthdate & phone number

--- src/main/java/org/pba/Main.java
+++ src/main/java/org/pba/Main.java
@@ -1,4 +1,22 @@
 package org.pba;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.Iterator;
+
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.cprGenerator("female"));
+        System.out.println(person.cprGenerator("male"));
+
+        System.out.println(person.cprGenerator("female"));
+        System.out.println(person.cprGenerator("male"));
+        System.out.println(person.cprGenerator("female"));
+        System.out.println(person.cprGenerator("male"));
+    }
+
 }
---
+++ src/main/java/org/pba/Person.java
@@ -0,0 +1,88 @@
+package org.pba;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+
+    private int cpr;
+
+    private int phoneNumber;
+
+    public Person(String firstName, String lastName, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+    }
+
+    public Person() {
+    }
+
+    public int cprGenerator(String gender){
+        LocalDate birthDate = birthDate();
+        int endRange = 9999;
+        int startRange = 1000;
+        int lastDigit;
+
+        if(gender.equals("female")){
+            Random evenRandom = new Random();
+            lastDigit = startRange + evenRandom.nextInt((endRange - startRange) / 2) * 2;
+        }else {
+            Random oddRandom = new Random();
+            lastDigit = startRange + oddRandom.nextInt((endRange - startRange) / 2) * 2 + 1;
+        }
+        return lastDigit;
+    }
+
+    public LocalDate birthDate(){
+        LocalDate startDate = LocalDate.of(1900, 1, 1);
+
+        int days = (int) ChronoUnit.DAYS.between(startDate, LocalDate.now());
+        int randomDays = new Random().nextInt(days);
+
+        LocalDate randomDate = startDate.plusDays(
+                ThreadLocalRandom.current().nextInt(randomDays));
+
+        return randomDate;
+    }
+
+    public int phoneNumberGenerator(){
+
+        int[] startDigitsCombinations = {2,30,31,40,41,42,50,51,52,53,
+                60,61,71,81,91,92,93,342,344,349,356,357,
+                359,362,365,366,389,398,431,441,462,466,
+                468,472,474,476,478,485,486,488,489,493,494,
+                495,496,498,499,542,543,545,551,552,556,571,
+                572,573,574,577,579,584,586,587,589,597,598,
+                627,629,641,649,658,662,663,664,665,667,692,
+                693,694,697,771,772,782,783,785,786,788,789,
+                826,827,829
+        };
+        Random random = new Random();
+
+        int i = random.nextInt(startDigitsCombinations.length);
+        int startDigit = startDigitsCombinations[i];
+        int endDigit;
+
+        if(startDigit == 2){
+            endDigit = new Random().nextInt(9999999);
+        } else if(startDigit <= 93){
+            endDigit = new Random().nextInt(999999);
+        } else{
+            endDigit = new Random().nextInt(99999);
+        }
+
+        return Integer.parseInt(String.valueOf(startDigit) + String.valueOf(endDigit));
+    }
+}
