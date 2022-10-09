261837f05c6db7bcd5936a4037a4e40b86ed63f2
erhan basaran <e.basaran.111@gmail.com>
2022-10-09 15:13:17 +0200

    random name extractor

--- src/main/java/org/pba/Main.java
+++ src/main/java/org/pba/Main.java
@@ -1,9 +1,46 @@
 package org.pba;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
+
+        Random random = new Random();
+        // parsing file "JSONExample.json"
+        Object obj = new JSONParser().parse(new FileReader("src/main/resources/jSONFiles/person-names.json"));
+
+        // typecasting obj to JSONObject
+        JSONObject jo = (JSONObject) obj;
+
+        // getting firstName and lastName
+        //String firstName = (String) jo.get("name");
+
+        JSONArray ja = (JSONArray) jo.get("persons");
+        int pos = random.nextInt(ja.size())+1;
+        JSONObject newJO = (JSONObject) ja.get(pos);
+        String name = (String) newJO.get("name");
+        String surname = (String) newJO.get("surname");
+        String gender = (String) newJO.get("gender");
+        System.out.println(newJO);
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(gender);
+        String persons = ja.toJSONString();
+        //System.out.println(persons);
+
+
+
 
     }
 }
